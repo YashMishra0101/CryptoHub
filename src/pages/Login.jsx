58f65ad907a41d5403af1973d67768a22f15438e
YashMishra0101 <yashrkm0011@gmail.com>
2025-07-23 16:06:50 +0530

    License added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,138 +1,150 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import bgimage from "../assets/loginlogoutbg.png";
+import { useNavigate } from "react-router";
+import toast, { Toaster } from "react-hot-toast";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/FirebaseConfig";
-import toast from "react-hot-toast";
-import Footer from "../components/Footer";
-const Login = () => {
+import { auth } from "../../src/firebase/FirebaseConfig";
+
+export const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // New state for login status
   const navigate = useNavigate();
 
-  const handleCheckboxChange = () => {
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.justLoggedIn) {
+      localStorage.setItem("user", JSON.stringify({ ...user, justLoggedIn: false }));
+    }
+  }, []);
+
+  const handleShowPassword = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      ); // Capture userCredential
-      localStorage.setItem("user", JSON.stringify(userCredential.user));
-      // Save login status in local storage
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      localStorage.setItem("user", JSON.stringify({ email: userCredential.user.email, justLoggedIn: true }));
       toast.success("Login Successful");
-      setIsLoggedIn(true); // Update login status state
-      navigate("/");
+      navigate("/houselistings");
     } catch (error) {
-      toast.error("Invalid email or password");
-      console.log(`Login failed: ${error.message}`);
+      toast.error("Please check your credentials.");
+      console.error(`${error.message}`);
     }
   };
 
   return (
-    <div>
-      <div className="bg-gray-50 dark:bg-gray-900 min-h-[40rem] overflow-hidden">
-        <div className="flex flex-col items-center justify-center px-6 py-7 mx-auto md:h-screen lg:py-0 relative">
-          <div className="w-full  rounded-lg shadow dborder md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700 mb-14 mt-10 ">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="md:text-center font-bold text-2xl text-white">
-                Log in to your account
-              </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@gmail.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    id="password"
-                    autoComplete="current-password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="flex items-center h-5">
+    <>
+      <Toaster /> {/* Only one toaster component for showing toasts */}
+      <div className="overflow-hidden min-h-screen md:mt-0 -mt-20">
+        <section className="relative">
+          <div className="absolute inset-0 min-h-screen">
+            <img
+              src={bgimage}
+              alt="Background"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 md:-mt-5 pt-[7rem] md:-pt-[7rem]">
+            <div className="w-full bg-white bg-opacity-50 rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 relative z-10">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                  Login to your account
+                </h1>
+                <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-600"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      className="bg-gray-50 bg-opacity-70 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Enter your email id"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-600"
+                    >
+                      Password
+                    </label>
+                    <div className="relative">
                       <input
-                        id="showpassword"
-                        type="checkbox"
-                        onChange={handleCheckboxChange}
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800 cursor-pointer"
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        placeholder="••••••••"
+                        className="bg-gray-50 bg-opacity-70 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required
                       />
                     </div>
-                    <div className="ml-2 mb-1  text-base cursor-pointer ">
-                      <label
-                        htmlFor="showpassword"
-                        className="text-blue-500 font-medium cursor-pointer "
-                      >
-                        Show password
-                      </label>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input
+                          id="showpassword"
+                          aria-describedby="showpassword"
+                          type="checkbox"
+                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 bg-opacity-70 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                          onClick={handleShowPassword}
+                        />
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label htmlFor="showpassword" className="text-gray-600">
+                          Show password
+                        </label>
+                      </div>
                     </div>
+                    <a
+                      href="/forgotpassword"
+                      className="text-sm font-medium text-primary-600 hover:underline text-gray-600"
+                    >
+                      Forgot password?
+                    </a>
                   </div>
-                  <div
-                    className="text-base font-medium text-blue-600 hover:underline ml-[3.5rem] lg:ml-[3.1rem] xl:ml-[3.1rem] text-center cursor-pointer pb-1"
-                    onClick={() => navigate("/resetPassword")}
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   >
-                    Forgot password
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700  focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mb-1"
-                >
-                  Log in
-                </button>
-                <p className="text-base font-light text-gray-500 dark:text-gray-400 cursor-pointer">
-                  Don’t have an account yet ?{" "}
-                  <span
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    onClick={() => navigate("/signup")}
-                  >
-                    Sign up
-                  </span>
-                </p>
-              </form>
+                    Login
+                  </button>
+                  <p className="text-sm text-gray-600">
+                    Don’t have an account yet?{" "}
+                    <span
+                      className="font-medium text-primary-600 hover:underline cursor-pointer"
+                      onClick={() => navigate("/registration")}
+                    >
+                      Register Yourself
+                    </span>
+                  </p>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
-      <div className="md:-mt-32 -mt-28">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
-
-export default Login;
